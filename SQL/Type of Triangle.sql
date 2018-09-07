8b75ea919b882e5796aed524af62948d652f74fd
Chetan <chetan.gadodia@eccella.com>
2018-09-07 16:35:09 +0530

    Hackerrank-sql-problems
    Basic SQL Solution

---
+++ SQL/Type of Triangle.sql
@@ -0,0 +1,16 @@
+SELECT
+CASE 
+     WHEN A=B AND B=C THEN 'Equilateral'
+
+     WHEN A+B<=C THEN 'Not A Triangle'
+     WHEN A+C<=B THEN 'Not A Triangle'
+     WHEN B+C<=A THEN 'Not A Triangle'
+
+     WHEN A = B AND A <> C then "Isosceles"
+     WHEN A = C AND A <> B then "Isosceles"
+     WHEN B = C AND A <> B then "Isosceles"
+     ELSE
+      'Scalene'  
+END    
+FROM
+TRIANGLES
